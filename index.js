a231388d0ac596f8948b247d4dc5a2fd4c7782f1
Manu Bharadwaj BN <102406248+Manu577228@users.noreply.github.com>
2024-02-08 14:01:45 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const search = () => {
+  let textToSearch = document.getElementById("text-to-search").value;
+  let paragraph = document.getElementById("paragraph");
+  textToSearch = textToSearch.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+
+  let pattern = new RegExp(`${textToSearch}`, "gi");
+
+  paragraph.innerHTML = paragraph.textContent.replace(
+    pattern,
+    (match) => `<mark>${match}</mark>`
+  );
+};
